1f0ea4529380ed6330f4dc81373303afc3503f62
Curtis Keller <cakeller42@gmail.com>
2021-06-19 15:59:04 -0500

    build fixes

--- InterviewStarter/Controllers/ContactController.cs
+++ InterviewStarter/Controllers/ContactController.cs
@@ -7,6 +7,7 @@ namespace InterviewStarter.Controllers
     [ApiController]
     public class ContactController : Controller
     {
+
         public ContactController() { }
 
         [HttpGet("{id}")]
