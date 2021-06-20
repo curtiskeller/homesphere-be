86a920526acc6932b99dc8bf348245bc569bf94f
Curtis Keller <cakeller42@gmail.com>
2021-06-19 21:06:08 -0500

    fix tests

--- InterviewTest.Test/ContactRepositoryTests.cs
+++ InterviewTest.Test/ContactRepositoryTests.cs
@@ -1,18 +1,53 @@
+using InterviewStarter.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InterviewStarter.Data.DataProviders;
+using System.Linq;
+using InterviewStarter.Data.Models;
+using System.Threading.Tasks;
 
 namespace InterviewTest.Test
 {
     [TestClass]
     public class ContactRepositoryTests
     {
-        [TestMethod]
-        public void ContactRepository_ReturnsOneContact()
+        private ContactRepository repo;
+
+        [TestInitialize]
+        public void testInit()
         {
+            repo = new ContactRepository(new ContactDataProvider());
         }
 
         [TestMethod]
-        public void ContactRepository_ReturnsAllContacts()
+        public async Task ContactRepository_ReturnsOneContact()
         {
+            var contact = await repo.Get(1);
+
+            Assert.AreEqual(1, contact.Id);
+            Assert.AreEqual("Ada Lovelace", contact.Name);
+            Assert.AreEqual("123 Babbage Court London England", contact.Address);
+        }
+
+        [TestMethod]
+        public async Task ContactRepository_ReturnsAllContacts()
+        {
+            var contacts = await repo.Get();
+
+            Assert.AreEqual(3, contacts.ToList().Count);
+        }
+
+        [TestMethod]
+        public async Task ContactRepository_AddsContact()
+        {
+            var contacts = await repo.Get();
+            Assert.AreEqual(3, contacts.ToList().Count);
+
+            await repo.Put(
+                new Contact { Id = 1, Name = "Curtis Keller", Address = "1000 Wally Street" });
+
+            var contactsAfterAdd = await repo.Get();
+
+            Assert.AreEqual(4, contactsAfterAdd.ToList().Count);
         }
     }
 }
